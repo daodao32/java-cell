680e867f110ea446cf801b0164a8fc715b8c206f
Jay Duan <dxj1984@gmail.com>
2023-02-18 19:34:10 -0500

    Create GlowingCell.java

---
+++ GlowingCell.java
@@ -0,0 +1,34 @@
+//Life Form 1: Glowing Bacteria
+//This life form is similar to the Mycoplasma but glows over time. 
+//The cell will start with a random color and as it lives on to the next generation, the color will slowly shift to a brighter shade.
+
+public class GlowingBacteria extends Organism {
+    private Color color;
+
+    public GlowingBacteria(int x, int y) {
+        super(x, y);
+        color = new Color((int)(Math.random() * 255), (int)(Math.random() * 255), (int)(Math.random() * 255));
+    }
+
+    @Override
+    public void update() {
+        int aliveNeighbors = countAliveNeighbors();
+        if (aliveNeighbors < 2) {
+            alive = false;
+        } else if (aliveNeighbors == 2 || aliveNeighbors == 3) {
+            alive = true;
+        } else {
+            alive = false;
+        }
+
+        if (alive) {
+            int red = color.getRed() + 10;
+            int green = color.getGreen() + 10;
+            int blue = color.getBlue() + 10;
+            if (red > 255) red = 255;
+            if (green > 255) green = 255;
+            if (blue > 255) blue = 255;
+            color = new Color(red, green, blue);
+        }
+    }
+}
